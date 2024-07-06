407196534c9ae0bc661aa1f2a9cac4a1dce996c9
Aliza Khan <alizaadi2@gmail.com>
2024-07-06 20:09:12 +0500

    login features with local storage access

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let confirmPassword = document.getElementById("confirmPassword");
-let passwordMatch = document.getElementById("passwordMatch");
-let passwordNotMatched = document.getElementById("passwordNotMatched");
-
 function loginUser() {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let confirmPassword = document.getElementById("confirmPassword");
+    let passwordMatch = document.getElementById("passwordMatch");
+    let passwordNotMatched = document.getElementById("passwordNotMatched");
+
     let userEmail = email.value.trim();
     let userPassword = password.value.trim();
     let confirmPasswordValue = confirmPassword.value.trim();
 
-    if (userEmail!== "" && userPassword.length >= 8 && userPassword === confirmPasswordValue) {
+
+    var storage = localStorage.getItem("MyUsers");
+    var arr = JSON.parse(storage);
+    if(!arr){
+        arr = [];
+    }
+    var MyUsers = {
+        email: email.value,
+        password: password.value
+    }
+    
+    arr.push(MyUsers);
+    localStorage.setItem("MyUsers", JSON.stringify(arr))
+
+console.log(userEmail.value);
+console.log(userPassword.value);
+
+
+    if (userEmail !== "" && userPassword.length >= 8 && userPassword === confirmPasswordValue) {
         passwordMatch.style.display = "block";
         passwordNotMatched.style.display = "none";
         Swal.fire({
@@ -20,21 +38,34 @@ function loginUser() {
             timer: 1500
         });
         // console.log("logged in");
-    } else if (userEmail!== "" && userPassword.length < 8) {
+    } else if (userEmail !== "" && userPassword.length < 8) {
         passwordLength.style.display = "block";
         passwordMatch.style.display = "none";
         passwordNotMatched.style.display = "none";
-    } else if (userEmail!== "" && userPassword!== confirmPasswordValue) {
+    } else if (userEmail !== "" && userPassword !== confirmPasswordValue) {
         passwordNotMatched.style.display = "block";
         passwordMatch.style.display = "none";
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
     }
+    userEmail = "";
+    userPassword = "";
 }
 
 
-
-function signupUser(){
+function signupUser() {
     let signupForm = document.getElementById("signupForm");
     signupForm.innerHTML = `
     `
     console.log("signed up")
 }
+
+
+// function addToLocalStorage(){
+//     let
+// }
