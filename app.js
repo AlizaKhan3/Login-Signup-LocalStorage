96100e914f9b2fff0dafdc85832bb0afac9b937f
Aliza Khan <alizaadi2@gmail.com>
2024-07-06 16:51:47 +0500

    updated

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ let email = document.getElementById("email");
 let password = document.getElementById("password");
 let confirmPassword = document.getElementById("confirmPassword");
 let passwordMatch = document.getElementById("passwordMatch");
+let passwordNotMatched = document.getElementById("passwordNotMatched");
 
 
 
-function loginUser(){
+
+function loginUser() {
+    let userEmail = email.value.trim();
+let userPassword = password.value.trim();
+    // if (userPassword === confirmPassword.value.trim()) {
+    //     passwordMatch.style.display = "block"
+    // }
+
+    if (userEmail != null && userPassword.length>=8 != null && userPassword === confirmPassword.value.trim()) {
+        passwordMatch.style.display = "block"
+        Swal.fire({
+            position: "top",
+            icon: "success",
+            title: "Welcome " + email.value.toLocaleUpperCase(),
+            showConfirmButton: false,
+            timer: 1500
+        });
         console.log("logged in");
-        
-        if(password.value === confirmPassword.value){
-            passwordMatch.style.display = "block"
-            // passwordMatch.style.fontSize = "6" + px
-            // passwordMatch.style.color = "green"
-            if(email.value.trim() != null && password.value.trim() != null){
-                Swal.fire({
-                    position: "top",
-                    icon: "success",
-                    title: "Welcome " + email.value.toLocaleUpperCase() ,
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-            }
-
-        }
-    else if(email.value.trim() != null && password.value.trim() != null && password.value !== confirmPassword.value ){
-
     }
+    // else if (userEmail != null && userPassword.length < 8 != null && userPassword !== confirmPassword.value.trim()) {
+    //     passwordNotMatched.style.display = "block"
+
+    // }
 }
